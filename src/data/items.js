ef83535c1aa3be7a4861421fac012297f16e92f4
martinmaresc <martinmarescr@gmail.com>
2023-05-17 08:23:22 -0600

    Create items.js
    const items added and its values along with find id fuction named getContact

---
+++ src/data/items.js
@@ -0,0 +1,18 @@
+export const items = [
+    {
+        id: '0',
+        name: 'Martin',
+        telephone: '449 000 00 00',
+        favorite: 'true'
+    },
+    {
+        id: '1',
+        name: 'Arturo',
+        telephone: '449 000 00 01',
+        favorite: 'false'
+    },
+];
+
+export function getContact(id) {
+    return items.find((item) => item.id === id);
+}
