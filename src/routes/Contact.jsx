681a7cab8ca6ec8212bf66fbad1a9e64e020abdc
martinmaresc <martinmarescr@gmail.com>
2023-05-19 09:37:15 -0600

    Updated Index, Contact
    Dinamic comunication between components added using useParams and useMemo

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,10 +1,19 @@
-import Menu from "../Components/Menu";
+
+import { useMemo } from "react";
+import { getContact } from "../data/items";
+import { useParams } from "react-router-dom";
 
 export default function Contact () {
+    const params = useParams();
+    const contact = useMemo(
+        () => getContact(params.contactid),
+        [params.contactid]
+    );
     return (
         <div>
-            <Menu />
-            <h1>This is contact</h1>
+            <h1>{contact.name}</h1>
+            <p>{contact.telephone}</p>
+            <p>{contact.favorite}</p>            
         </div>
     )
 }
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Menu from "../Components/Menu";
+import { items } from "../data/items";
+import { Link } from 'react-router-dom';
 
 export default function Home () {
     return(
@@ -8,6 +10,13 @@ export default function Home () {
         <h1>This is home</h1>
 
         <div>
+          <div>
+            {items.map((item) => (
+              <div>
+                <Link to={`/contacts/${item.id}`}>{item.name}</Link>
+              </div>
+            ))}
+          </div>
         <Outlet />
         </div>
       </div> 
